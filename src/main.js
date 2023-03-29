59695e9fa760876a50ffbcf5a0353a70299e548e
George Rush <gmrush04@gmail.com>
2023-03-29 17:14:08 -0400

    feat: broken melons

--- src/main.js
+++ src/main.js
@@ -3,20 +3,17 @@ import * as ml from "../lib/melonjs.module.js";
 ml.device.onReady(function () {
 
 	//Initialize video
-	if (!ml.video.init(1280, 720, {parent : "screen", scale : "auto"})) {
+	if (!ml.video.init(1600, 900, {parent : "screen", scale : "auto"})) {
 		alert("Your browser does not support HTML5 canvases.");
 		return;
 	}
 
-	ml.game.world.backgroundColor.parseCSS("#000000");
-
-	ml.game.world.addChild(new ml.Text(610, 360, {
-		font: "Arial",
-		size: 160,
-		fillStyle: "#FFFFFF",
-		textBaseline: "middle",
-		textAlign: "center",
-		text: "Hello World!"
-	}));
+	main();
 
 });
+
+function main() { //Make the rest of the game lol
+	var world = new ml.World(0, 0, 1600, 900);
+
+	world.backgroundColor.setColor(0, 0, 0); //This also doesn't work
+}
