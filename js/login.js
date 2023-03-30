abc7bfd7b0e9c53f9db10f4bfda7360749d91635
AncientRuins <nishwanthg@hotmail.com>
2023-03-29 21:04:55 -0400

    Started work on login validation
    Started writing the function, but ran into an issue with the backend database.

--- js/login.js
+++ js/login.js
@@ -1,3 +1,10 @@
-function checkAttempt(res) {
-  alert(res);
+async function validateSignIn() {
+	const userName = document.getElementById("createUserText").value;
+	console.log(userName);
+	const response = await fetch("http://localhost:9000/users/check", {
+		method: "GET",
+		mode: "cors",
+		params: JSON.stringify(userName),
+	});
+	console.log(response);
 }
